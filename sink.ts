2c89c0d6196946cbd03ef9183ae64c8275fe5e6e
Ben Bryant <brbryant2639@gmail.com>
2020-03-30 15:32:02 -0500

    completed objectives

--- sink.ts
+++ sink.ts
@@ -44,3 +44,32 @@ patrons.forEach(per => {
   checkAge(per);
 });
 
+let veggies = [
+  { rank: 1, type: "Celery" },
+  { rank: 2, type: "Broccoli" },
+  { rank: 3, type: "Cauliflower" },
+  { rank: 4, type: "Zucchini" },
+  { rank: 5, type: "Asparagus" }
+];
+
+console.log(`Favorite Veggies:`);
+
+// Array function that console.logs each value in array
+veggies.forEach(veggie => {
+  console.log(`${veggie.rank}: ${veggie.type}`);
+});
+
+// Function that takes a string and returns length
+function getLength(word: string) {
+  return word.length;
+}
+
+// Declares variable wLength that stores length of Hello World
+let wLength = getLength(`Hello World`);
+
+// Conditional statement that console.logs based on even or odd wLength
+if (wLength % 2) {
+  console.log(`The world is nice and even!`);
+} else {
+  console.log(`The world is an odd place!`);
+}
